dd69b95f8d6754af04f7647df0bfa5a96cb94c04
dagboujhatem <dagboujhatem@gmail.com>
2021-02-16 15:08:01 +0100

    mail template

--- routes/mailAPI.js
+++ routes/mailAPI.js
@@ -1,8 +1,38 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
+const fs = require('fs');
+const path = require('path');
+const ejs = require('ejs');
 const router = express.Router();
 
 // les API
-router.post('/send-html', (req, res)=>{
+router.post('/send-html/:name', async(req, res)=>{
+
+    // 1. create transporter 
+    const transporter = nodemailer.createTransport(
+        {
+        service: 'gmail',
+        auth: {
+            user: "***************@gmail.com",
+            pass: "***************"
+        }
+    });
+    // 2. load HTML template
+    const template = fs.readFileSync(path.resolve('./common/mail_templates', 'register_mail.html'), {encoding: 'utf-8'});
+    console.log(template);
+    const html = ejs.render(template, {
+        name: req.params.name
+    })
+    console.log(html);
+
+    // 3. create mail options
+    let info = await transporter.sendMail({
+        from: '"Fred Foo 👻" <dagboujhatem@gmail.com>', // sender address
+        to: "sofien.gallas@gmail.com, dagboujhatem@gmail.com", // list of receivers
+        subject: "Hello ✔", // Subject line
+        html: html, // html body
+    });
+
     res.json({message : "E-mail send successfully!"})
 });
 
